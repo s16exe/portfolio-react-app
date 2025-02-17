c19a8e879e2a06b5d7b4f6954172f9ca893d92f4
K S Subramanya Char <subramanyachar03@outlook.com>
2025-02-17 22:28:46 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,11 +4,13 @@ export default function Experience({ role, company, duration, children }) {
   return (
     <div className=" my-5 font-medium text-[16px] ">
       <div className="w-full flex justify-between mb-2">
-        <p className="w-[70%]">
+        <p className="w-[75%] text-center xxs:text-left">
           → {role} <br />
           <i className="ml-3">@{company}</i>
         </p>
-        <p className="text-[15px] w-[160px]"> {duration}</p>
+        <p className="text-[15px] w-[160px] text-center xxs:text-left">
+          {duration}
+        </p>
       </div>
       {children}
     </div>
